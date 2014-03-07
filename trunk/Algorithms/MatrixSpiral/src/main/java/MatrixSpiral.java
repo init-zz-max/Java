24ef8be9ca75615b7550fdf6993701eb889b8367
mzavgorodny <initmax@gmail.com>
2014-03-07 12:21:37 +0200

    MatrixSpiral optimization

--- trunk/Algorithms/MatrixSpiral/src/main/java/MatrixSpiral.java
+++ trunk/Algorithms/MatrixSpiral/src/main/java/MatrixSpiral.java
@@ -10,8 +10,8 @@ public class MatrixSpiral {
 
     public void spiralExecution(String[][] matrix) {
 
-        int rows = matrix.length-1;
-        int columns = matrix[0].length-1;
+        int rows = matrix.length - 1;
+        int columns = matrix[0].length - 1;
 
 /*
         {"1",  "2",  "3",  "4",  "5"},
@@ -26,35 +26,35 @@ public class MatrixSpiral {
         */
 
         for (int i = 0; i < rows; i++) {
-            printRow(matrix, i, columns - i, i);
-            printColumn(matrix, i, rows - i, columns - i);
-            printRowBack(matrix, columns - i, i, rows - i);
-            printColumnUp(matrix, columns - i, i, i);
+            printRow(matrix, i, columns - i);
+            printColumn(matrix, i, rows - i);
+            printRowBack(matrix, columns - i, i);
+            printColumnUp(matrix, columns - i, i);
         }
 
     }
 
-    private void printRow(String[][] matrix, int startColumnt, int endColumnt, int rowNumber) {
+    private void printRow(String[][] matrix, int startColumnt, int endColumnt) {
         for (int i = startColumnt; i <= endColumnt; i++) {
-            System.out.println(matrix[rowNumber][i]);
+            System.out.println(matrix[startColumnt][i]);
         }
     }
 
-    private void printColumn(String[][] matrix, int startRow, int endRow, int columnNumber) {
+    private void printColumn(String[][] matrix, int startRow, int endRow) {
         for (int j = startRow + 1; j <= endRow; j++) {
-            System.out.println(matrix[j][columnNumber]);
+            System.out.println(matrix[j][endRow]);
         }
     }
 
-    private void printRowBack(String[][] matrix, int startColumnt, int endColumnt, int rowNumber) {
+    private void printRowBack(String[][] matrix, int startColumnt, int endColumnt) {
         for (int j = startColumnt - 1; j >= endColumnt; j--) {
-            System.out.println(matrix[rowNumber][j]);
+            System.out.println(matrix[startColumnt][j]);
         }
     }
 
-    private void printColumnUp(String[][] matrix, int startRow, int endRow, int columnNumber) {
+    private void printColumnUp(String[][] matrix, int startRow, int endRow) {
         for (int j = startRow - 1; j > endRow; j--) {
-            System.out.println(matrix[j][columnNumber]);
+            System.out.println(matrix[j][endRow]);
         }
     }
 
